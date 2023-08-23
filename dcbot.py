b946e542b53f3f75593adecc68412e742a4cefa7
kamil5b <zee312kamilerz@gmail.com>
2023-08-23 17:26:00 +0700

    accepting link and better logging system

--- dcbot.py
+++ dcbot.py
@@ -6,7 +6,17 @@ print("BOT BERHASIL")
 
 load_dotenv()
 
-with open('template.txt') as file:
+async def log_print(msg,txt,author=None, msg_send=False, file_name="dm.log"):
+    current_datetime = datetime.now()
+    current_date_time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
+    print(f'[{current_date_time}]{author}: {txt}\n')
+    if msg_send:
+        await msg.channel.send(txt)
+    f = open(file_name, "a")
+    f.write(f'[{current_date_time}]{author}: {txt}\n')
+    f.close()
+
+with open('template.txt',encoding="utf-8") as file:
     template = file.read()
 
 client = discord.Client()
@@ -18,45 +28,45 @@ async def on_message(msg):
     if msg.author == client.user:
         return
     if not msg.guild:
-        
-        current_datetime = datetime.now()
-        current_date_time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
-        print(f"[{current_date_time}]{msg.author.name}:{msg.content}\n")
-        f = open("dm.log", "a")
-        f.write(f"[{current_date_time}]{msg.author.name}:{msg.content}\n")
-        f.close()
+
+        await log_print(msg,msg.content,author=msg.author.name)
         print("========================================")
         try:
             if msg.content.startswith('!channel_id'):
-                channel_id = int(msg.content.split()[1])
+                tmp = msg.content.split()[1]
+                channel_id = 0
+                try:
+                    channel_id = int(tmp)
+                except:
+                    tmp_arr = tmp.split(sep="/")
+                    try:
+                        channel_id = int(tmp_arr[5])
+                    except:
+                        await log_print(msg,"Error occured.",author=f"Response to {msg.author.name}",msg_send=True)
+                        return
                 with open('channels.list') as file:
                     channels = [int(line.rstrip()) for line in file]
                 if channel_id in channels:
-                    print(f'[{current_date_time}]Response to {msg.author.name}: Sorry, The channel with ID {channel_id} already registered\n')
-                    await msg.channel.send(f'Sorry, The channel with ID {channel_id} already registered')
-                    f = open("dm.log", "a")
-                    f.write(f'[{current_date_time}]Response to {msg.author.name}: Sorry, The channel with ID {channel_id} already registered\n')
-                    f.close()
+                    await log_print(msg,f"Sorry, The channel with ID {channel_id} already registered\n",author=f"Response to {msg.author.name}",msg_send=True)
                 else:
                     try:
+                        #Check if channel can be registered
                         ch = client.get_channel(channel_id)
                         await ch.send(template)
+
+                        #If can, then add to list
                         f = open("channels.list", "a")
                         f.write("\n"+str(channel_id))
                         f.close()
-                        print(f'[{current_date_time}]Response to {msg.author.name}: Your channel successfully registered with ID:{channel_id}\n')
-                        await msg.channel.send(f'Your channel successfully registered with ID:{channel_id}')
-                        f = open("dm.log", "a")
-                        f.write(f'[{current_date_time}]Response to {msg.author.name}: Your channel successfully registered with ID:{channel_id}\n')
-                        f.close()
+
+                        await log_print(msg,f'Your channel successfully registered with ID:{channel_id}',author=f"Response to {msg.author.name}",msg_send=True)
+
                     except:
-                        print(f'[{current_date_time}]Response to {msg.author.name}: Sorry, we couldn\'t access your channel with ID:{channel_id}\n')
-                        await msg.channel.send(f'Sorry, we couldn\'t access your channel with ID:{channel_id}')
-                        f = open("dm.log", "a")
-                        f.write(f'[{current_date_time}]Response to {msg.author.name}: Sorry, we couldn\'t access your channel with ID:{channel_id}\n')
-                        f.close()
+                        await log_print(msg,f'Sorry, we couldn\'t access your channel with ID:{channel_id}',author=f"Response to {msg.author.name}",msg_send=True)
 
             elif msg.content.startswith('!report'):
+                current_datetime = datetime.now()
+                current_date_time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
                 txt = msg.content.replace('!report',"\n["+current_date_time+"]"+msg.author.name+":")
                 f = open("reports.log", "a")
                 f.write(txt)
@@ -64,13 +74,12 @@ async def on_message(msg):
                 print(txt)
                 await msg.channel.send("Thank you for reporting!")
             elif msg.author != client.user:
-                print(f"[{current_date_time}]Response to {msg.author.name}: template response\n")
+                await log_print(msg,'Template response',author=f"Response to {msg.author.name}")
                 await msg.channel.send(template)
-                f = open("dm.log", "a")
-                f.write(f'[{current_date_time}]Response to {msg.author.name}: template response\n')
-                f.close()
         except discord.errors.Forbidden:
             pass
+        except:
+            await log_print(msg, 'Error occured.',author=f"Response to {msg.author.name}",msg_send=True)
     else:
         pass
     
